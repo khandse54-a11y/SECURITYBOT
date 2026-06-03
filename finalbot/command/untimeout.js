b194abb3ce22b87416433b5ab74f0a046e1d8b8c
khandse54-a11y <khandse54@gmail.com>
2026-06-03 12:41:28 +0530

    Update untimeout.js

--- finalbot/command/untimeout.js
+++ finalbot/command/untimeout.js
@@ -1,14 +1,16 @@
-const { isWhitelisted } = require('../utils/whitelist');
+// commands/untimeout.js
+const store = require('../utils/store');
+const { successEmbed, errorEmbed } = require('../utils/embeds');
 
 module.exports = {
   name: 'untimeout',
   async execute(message, args, client) {
-    if (!isWhitelisted(message.author.id))
-      return message.reply('❌ You are not authorized.');
+    if (!store.isWhitelisted(message.author.id) && message.author.id !== message.guild.ownerId)
+      return message.reply({ embeds: [errorEmbed('No Permission', 'You are **not whitelisted** to use this command.')] });
 
     const target = message.mentions.members.first()
       || await message.guild.members.fetch(args[0]).catch(() => null);
-    if (!target) return message.reply('❌ User not found. Usage: `!untimeout @user`');
+    if (!target) return message.reply({ embeds: [errorEmbed('User Not Found', 'Usage: `!untimeout @user`')] });
 
     try {
       await Promise.all([
@@ -16,9 +18,9 @@ module.exports = {
         target.timeout(null),
       ]);
       await target.user.send(`✅ Your timeout has been removed in **${message.guild.name}**.`).catch(() => {});
-      await message.channel.send(`✅ **${target.user.tag}** timeout removed.`);
+      await message.channel.send({ embeds: [successEmbed('Timeout Removed', `**${target.user.tag}**'s timeout has been removed.`)] });
     } catch (err) {
-      message.channel.send(`❌ Failed: ${err.message}`);
+      message.channel.send({ embeds: [errorEmbed('Failed', err.message)] });
     }
   }
 };
