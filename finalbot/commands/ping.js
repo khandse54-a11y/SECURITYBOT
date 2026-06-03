b7f724d872bb47da42f934f9778ac883239ebf85
khandse54-a11y <khandse54@gmail.com>
2026-06-03 12:28:58 +0530

    Delete finalbot directory

--- finalbot/commands/ping.js
+++
@@ -1,9 +0,0 @@
-// commands/ping.js
-module.exports = {
-  name: 'ping',
-  async execute(message, args, client) {
-    const sent = await message.reply('🏓 Pinging...');
-    const latency = sent.createdTimestamp - message.createdTimestamp;
-    sent.edit(`🏓 **Pong!**\n📶 Bot Latency: \`${latency}ms\`\n💡 API Latency: \`${Math.round(client.ws.ping)}ms\``);
-  }
-};
